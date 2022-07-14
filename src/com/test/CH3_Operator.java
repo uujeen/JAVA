1a2205f8cd76717e28dc976ad7b155f570070661
uujeen <rkddmlwls2@naver.com>
2022-07-14 14:24:50 +0900

    CH3_Operator

--- src/com/test/CH3_Operator.java
+++ src/com/test/CH3_Operator.java
@@ -2,10 +2,14 @@ package com.test;
 
 public class CH3_Operator {
 	public static void main(String[] args) {
-		Operator1();
-		Operator2();
-		Operator3();
-		Operator4();
+		Operator1(); // 증감연산자의 계산 순
+		Operator2(); // 자료형의 overflow 시 값의 변화
+		Operator3(); // char 형의 int 값을 더했을 때 반환
+		Operator4(); // 자료형 변환 시 주의할 점
+		Operator5(); // math.round()의 반올림
+		Operator6(); // 나머지 연산자시 피연산자의 부호 생략
+		Operator7(); // A.equals("B") 문자열 A, B 비교 bool 값 반
+		Operator8(); // 쉬프트연산자의 계
 	}
 	
 	public static void Operator1(){
@@ -48,5 +52,31 @@ public class CH3_Operator {
 		// -> char c2 = (char)(c2+1); 으로 해야 가능
 		char c2 = 'a' + 1; //리터럴 간의 연산이기 떄문에 에러발생x
 		System.out.println(c2);
+	}
+	
+	public static void Operator5() {
+		double pi = 3.141592;
+		double ShortPi = Math.round(pi*1000)/ 1000.0; // 1000 나눌 경우 3.0 출력
+		System.out.println(ShortPi);
+	}
+	
+	public static void Operator6() {
+		System.out.println(10%8);
+		System.out.println(10%-8);
+	}
+	
+	public static void Operator7() {
+		String str = "abc";
+		
+		boolean result = str.equals("str"); // str과 equals()내에 있는 문자열과 비교 bool값 반환
+		System.out.println(result);
+		
+	}
+	
+	public static void Operator8() {
+		String x = "x";
+		String n = "n";
+		System.out.printf("%s<<%s = %s*2^%s%n", x, n, x, n);
+		System.out.printf("%s>>%s = %s/2^%s", x, n, x, n);
 	}
 }
---
+++ src/com/test/CH4_CaseandRepeat.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class CH4_CaseandRepeat {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void Example(){
+		
+	}
+	
+}
